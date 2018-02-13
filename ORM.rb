5490acaf26f3546ae1547bb634d0f63b216320dd
SChernuyk <sergey@taganteam.com>
2018-02-13 15:33:10 +0300

    improve code readability

--- ORM.rb
+++ ORM.rb
@@ -4,35 +4,38 @@ require 'pg'
 class GeneralPostgres
 
 
-  def initialize
+  def initialize(options = {})
+    @dbname    = options[:dbname ]
+    @user      = options[:user]
+    @password  = options[:password]
   end
 
   def create_table
-    @connect = PG.connect :dbname => 'sergey', :user => 'sergey', :password => '12345'
-    @connect.exec("CREATE TABLE vapeshop(id integer PRIMARY KEY NOT NULL,name varchar(50) NOT NULL,tasty varchar(50) NOT NULL,type varchar(50) NOT NULL);")
+    connect = connect(@dbname,@user,@password)
+    connect.exec("CREATE TABLE vapeshop(id integer PRIMARY KEY NOT NULL,name varchar(50) NOT NULL,tasty varchar(50) NOT NULL,type varchar(50) NOT NULL);")
+    connect.close
   end
   def add_value(options)
-    @connect = PG.connect :dbname => 'sergey', :user => 'sergey', :password => '12345'
-    @connect.exec("INSERT INTO vapeshop (id,name,tasty,type) values ('#{options[:id]}', '#{options[:name]}', '#{options[:tasty]}', '#{options[:type]}')")
-    @connect.close
+    connect = connect(@dbname,@user,@password)
+    connect.exec("INSERT INTO vapeshop (id,name,tasty,type) values ('#{options[:id]}', '#{options[:name]}', '#{options[:tasty]}', '#{options[:type]}')")
+    connect.close
   end
 
   def delete_value(id)
-    @connect = PG.connect :dbname => 'sergey', :user => 'sergey', :password => '12345'
-    @connect.exec("DELETE FROM vapeshop WHERE id=#{id}")
-    @connect.close
+    connect = connect(@dbname,@user,@password)
+    connect.exec("DELETE FROM vapeshop WHERE id=#{id}")
+    connect.close
   end
 
   def update_value(options)
-    @connect = PG.connect :dbname => 'sergey', :user => 'sergey', :password => '12345'
-    @connect.exec("UPDATE vapeshop SET name = '#{options[:name]}',tasty = '#{options[:tasty]}',type = '#{options[:type]}' WHERE id = #{options[:id]}")
-    @connect.close
+    connect = connect(@dbname,@user,@password)
+    connect.exec("UPDATE vapeshop SET name = '#{options[:name]}',tasty = '#{options[:tasty]}',type = '#{options[:type]}' WHERE id = #{options[:id]}")
+    connect.close
   end
 
   def select_value
-    @connect = PG.connect :dbname => 'sergey', :user => 'sergey', :password => '12345'
-
-    @connect.exec("SELECT * FROM vapeshop") do |result|
+    connect = connect(@dbname,@user,@password)
+    connect.exec("SELECT * FROM vapeshop") do |result|
       result.each do |x|
       #id    = x["id"]
       # name  = x["name"]
@@ -42,15 +45,18 @@ class GeneralPostgres
        p x
       end
     end
-
-
-    @connect.close
-
+    connect.close
   end
 
+  private
+
+  def connect(userdb,username,pass)
+    connect = PG.connect :dbname => userdb, :user => username, :password => pass
+  end
 end
 
-application = GeneralPostgres.new
+application = GeneralPostgres.new(:dbname => 'sergey', :user => 'sergey', :password => '12345')
+
 #application.create_table
 #application.add_value(:id => "0", :name => "vodka", :tasty => "malina", :type => "krasnaya")
 # application.add_value(:id => "1", :name => "pivo", :tasty => "yabloko", :type => "svetloye")
