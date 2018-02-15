4e94a771641d969d9b27e9d0378e2dcf8fe84b70
SChernuyk <sergey@taganteam.com>
2018-02-15 15:45:16 +0300

    workable ORM

--- ORM.rb
+++ ORM.rb
@@ -1,70 +1,62 @@
 require 'pg'
 
-
 class GeneralPostgres
 
-
-  def initialize(options = {})
-    @dbname    = options[:dbname ]
-    @user      = options[:user]
-    @password  = options[:password]
+  def connect
+    connect = PG.connect :dbname => 'sergey', :user => 'sergey', :password => '12345'
   end
 
-  def create_table
-    connect = connect(@dbname,@user,@password)
-    connect.exec("CREATE TABLE vapeshop(id integer PRIMARY KEY NOT NULL,name varchar(50) NOT NULL,tasty varchar(50) NOT NULL,type varchar(50) NOT NULL);")
-    connect.close
-  end
   def add_value(options)
-    connect = connect(@dbname,@user,@password)
-    connect.exec("INSERT INTO vapeshop (id,name,tasty,type) values ('#{options[:id]}', '#{options[:name]}', '#{options[:tasty]}', '#{options[:type]}')")
+    values = Array.new
+    colums = Array.new
+    options.each{|key, value| colums.push(key.to_s); values.push("'#{value.to_s}'")}
+    values = values*", "
+    colums = colums*", "
+    p  colums, values
+    connect.exec("INSERT INTO #{name_table}(#{colums}) values (#{values})")
     connect.close
   end
 
   def delete_value(id)
-    connect = connect(@dbname,@user,@password)
-    connect.exec("DELETE FROM vapeshop WHERE id=#{id}")
+    connect.exec("DELETE FROM #{name_table} WHERE id=#{id}")
     connect.close
   end
 
-  def update_value(options)
-    connect = connect(@dbname,@user,@password)
-    connect.exec("UPDATE vapeshop SET name = '#{options[:name]}',tasty = '#{options[:tasty]}',type = '#{options[:type]}' WHERE id = #{options[:id]}")
+  def update_value(options,id)
+    updates = Array.new
+    options.each {|key, new_values| updates.push("#{key} = '#{new_values}'")}
+    updates = updates*", "
+    connect.exec("UPDATE #{name_table} SET #{updates}  WHERE id = #{id}")
     connect.close
   end
 
   def select_value
-    connect = connect(@dbname,@user,@password)
-    connect.exec("SELECT * FROM vapeshop") do |result|
-      result.each do |x|
-      #id    = x["id"]
-      # name  = x["name"]
-      # tasty = x["tasty"]
-      # type  = x["type"]
-      #  puts " #{id} #{name} #{tasty} #{type} "
+    connect.exec("SELECT * FROM #{name_table}") do |result|
+      result.each {|x|
        p x
-      end
+      }
     end
     connect.close
+    end
+
   end
 
-  private
-
-  def connect(userdb,username,pass)
-    connect = PG.connect :dbname => userdb, :user => username, :password => pass
+  def name_table
+    name_table = self.class.to_s + "s"
   end
+
+class Good < GeneralPostgres
+
 end
 
-application = GeneralPostgres.new(:dbname => 'sergey', :user => 'sergey', :password => '12345')
-
-#application.create_table
-#application.add_value(:id => "0", :name => "vodka", :tasty => "malina", :type => "krasnaya")
-# application.add_value(:id => "1", :name => "pivo", :tasty => "yabloko", :type => "svetloye")
-# application.add_value(:id => "2", :name => "liker", :tasty => "vishnya", :type => "kreplenii")
-# application.add_value(:id => "3", :name => "samogon", :tasty => "orex", :type => "korichnevii")
-application.select_value
-#application.delete_value(1)
-application.update_value(:id =>0,:name =>"absent",:tasty => "tarxun", :type =>"zelenaya")
-application.select_value
+  a = { "item" => "xbox", "price" => "18000", "weight" => "6"}
+  id=3
+  b = {"item" => "xboxone", "price" => "20000", "weight" => "7"}
+  application = Good.new
+  #application.add_value(a)
+  # application.select_value
+  #application.delete_value(2)
+   application.update_value(b, id)
+  # application.select_value
 
 
